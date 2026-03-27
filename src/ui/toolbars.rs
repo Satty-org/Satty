4c5dc0c0dee8db4270cd674ad4b7ccf2f8102b58
Rene D. Obermueller <cmdrrdo@gmail.com>
2026-03-27 20:52:52 +0100

    feat: add pixelate and "independent" pixelate
    Closes: #90
    
    This implements a pixelate tool with two modes
    - regular mode with a normal block pixelation, this is, depending on
      selected area and image resolution, reversible
    - "independent mode" that takes data from outside the selected area, the
      area is entirely overwritten by this. Then, the regular pixelation
    creates a block-pixelate look.
    
    Some things are still open:
    [ ] figure out a better icon - help needed please
    [ ] reach out to flameshot guys regarding credits
    [ ] figure out if BLOCKSIZE is good at a value of 32. This may actually need
    to be adjustable.
    
    Known limitations:
    - in independent mode, canvas background may contribute to the mix of
      pixels if the selection is started at image origin
    - in idependent mode, if the selection "leaves" Satty's window, nothing
    happens. We would be lacking one fringe in that instance.
    
    This PR was inspired by
    - https://github.com/flameshot-org/flameshot/pull/3765/changes
    - #468 by kikeijuu (superseded by this PR)
    
    Regarding the credits, the idea with taking "fringes" was taken from
    said PR, but I reimplemented the gist of it according to my
    understanding. I did, for example, skip any additional noise which
    might be suitable for adding later. Also, actually iterating the source
    and target data works a bit different due to the objects used. I don't
    think we need to talk about a separate licence for this bit, but we'll
    see what the flameshot guys say about this.

--- src/ui/toolbars.rs
+++ src/ui/toolbars.rs
@@ -293,11 +293,18 @@ impl SimpleComponent for ToolsToolbar {
                 icon_name: "number-circle-1-regular".into(),
                 tooltip: None,
             }],
-            vec![GroupableTool {
-                tool: Tools::Blur,
-                icon_name: "drop-regular".into(),
-                tooltip: None,
-            }],
+            vec![
+                GroupableTool {
+                    tool: Tools::Pixelate,
+                    icon_name: "tetris-app-regular".into(),
+                    tooltip: None,
+                },
+                GroupableTool {
+                    tool: Tools::Blur,
+                    icon_name: "drop-regular".into(),
+                    tooltip: None,
+                }
+            ],
             vec![GroupableTool {
                 tool: Tools::Highlight,
                 icon_name: "highlight-regular".into(),
