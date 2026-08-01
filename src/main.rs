e17d879ee64416127928bc60d7fd8efeac5202f4
Rene D. Obermueller <cmdrrdo@gmail.com>
2026-08-01 15:41:54 +0200

    fix clippy hint

--- src/main.rs
+++ src/main.rs
@@ -534,10 +534,10 @@ fn run_satty() -> Result<()> {
         Ok(mut temp_dir) => {
             // take is sufficient to have the dir deleted when it's dropped.
             // But that would hide any errors, so use explicit close instead.
-            if let Some(dir) = temp_dir.take() {
-                if let Err(e) = dir.close() {
-                    eprintln!("Failed to close temporary directory: {}", e);
-                }
+            if let Some(dir) = temp_dir.take()
+                && let Err(e) = dir.close()
+            {
+                eprintln!("Failed to close temporary directory: {}", e);
             }
         }
         Err(e) => {
